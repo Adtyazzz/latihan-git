ae794327b540c4219744cefe9a0e16d7254eb40e
Adtyazzz <adityaasaputraa2005@gmail.com>
2026-06-24 15:13:51 +0800

    terbaru

--- array.js
+++ array.js
@@ -1,44 +1,44 @@
 // contoh penggunaan array
-// var angka = [2, 5, 1, 3, 4]
+var angka = [2, 5, 1, 3, 4]
 // console.log(angka)
-// console.log(angka[2])
+console.log(angka[2])
 
 
 // kumpulan method pada array
 
 // // method length
-// console.log(angka.length)
+console.log(angka.length)
 
 // method push
-// console.log("sebelum push " + angka)
-// angka.push(9)
-// console.log("setelah push " + angka)
+console.log("sebelum push " + angka)
+angka.push(9)
+console.log("setelah push " + angka)
 
 //method pop
-// console.log("sebelum pop " + angka)
-// angka.pop()
-// console.log("setelah pop " + angka)
+console.log("sebelum pop " + angka)
+angka.pop()
+console.log("setelah pop " + angka)
 
 // method unshift
-// console.log("sebelum unshift " + angka)
-// angka.unshift(7)
-// console.log("setelah unshift " + angka)
+console.log("sebelum unshift " + angka)
+angka.unshift(7)
+console.log("setelah unshift " + angka)
 
 // method shift
-// console.log("sebelum shift " + angka)
-// angka.shift(7)
-// console.log("setelah shift " + angka)
+console.log("sebelum shift " + angka)
+angka.shift(7)
+console.log("setelah shift " + angka)
 
 
 var peserta = ["Andra", "Taufik", "Adit", "Debi"]
 // console.log(peserta)
 
 //method join
-// console.log("sebelum join")
-// console.log(peserta)
+console.log("sebelum join")
+console.log(peserta)
 
-// console.log("setelah join")
-// console.log(peserta.join("-"))
+console.log("setelah join")
+console.log(peserta.join("-"))
 
 // split
 var nama = "Muhammad Aditya Dwi Saputra"
@@ -46,3 +46,26 @@ console.log("sebelum split")
 console.log(nama)
 console.log("sesudah split")
 console.log(nama.split(" ").length)
+
+// method sort
+console.log("sebelum di sort")
+console.log(peserta)
+console.log("setelah di sort")
+console.log(peserta.sort())
+console.log("setelah di reverse")
+console.log(peserta.reverse())
+
+
+//method slice
+console.log("sebelum slice")
+console.log(peserta)
+console.log("setelah slice")
+var newPeserta = peserta.slice(2,4)
+console.log(newPeserta)
+console.log(peserta)
+
+// method splice
+console.log("sebelum splice")
+console.log(peserta)
+var newPeserta = peserta.splice(2,0,"heldi","huda")
+console.log(peserta)
--- looping.js
+++ looping.js
@@ -1,23 +1,23 @@
-// var jumlah = 0;
-// for(var deret = 5; deret > 0; deret--){
-//     jumlah += deret
-//     console.log("jumlah saat ini " + jumlah)
-// }
-// console.log("jumlah terakhir :" + jumlah)
+var jumlah = 0;
+for(var deret = 5; deret > 0; deret--){
+    jumlah += deret
+    console.log("jumlah saat ini " + jumlah)
+}
+console.log("jumlah terakhir :" + jumlah)
 
 
 // while loop (while = selagi)
 
-// var flag = 1
-// while(flag < 10){
-//     console.log('iterasi ke-' + flag);
-//     flag++;
-// }
-// var flag2 = 9;
-// do {
-//     console.log('iterasi ke-' + flag2);
-//     flag2--;
-// } while(flag2 > 0)
+var flag = 1
+while(flag < 10){
+    console.log('iterasi ke-' + flag);
+    flag++;
+}
+var flag2 = 9;
+do {
+    console.log('iterasi ke-' + flag2);
+    flag2--;
+} while(flag2 > 0)
 
 for (var angka = 2; angka <= 10 ; angka+=2) {
     console.log('iterasi for ke : ' + angka );
